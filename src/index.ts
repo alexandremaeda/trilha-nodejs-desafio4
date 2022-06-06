13115b5313f30ee8aeaf2a63bb479e58a83cf7ec
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-06-06 16:45:02 -0300

    swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use("/users", usersRoutes);
 
